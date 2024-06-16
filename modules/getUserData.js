7a024e2d0f860ea129e3ed4b5ac3f9d6cb2bca16
PurinPintakhiew <dev.purin@gmail.com>
2024-06-16 16:52:07 +0700

    feat : add sort data

--- modules/getUserData.js
+++ modules/getUserData.js
@@ -9,36 +9,36 @@ const getUserData = async (username) => {
 
         const userResponse = await fetch(`https://api.github.com/users/${username}`);
 
-        if (!userResponse.ok) {
-            throw new Error(`Failed to fetch user data: ${userResponse.statusText}`);
+        if (!userResponse?.ok) {
+            throw new Error(`Failed to fetch user data: ${userResponse?.statusText}`);
         }
 
-        const userData = await userResponse.json();
+        const userData = await userResponse?.json();
 
-        if (!userData.repos_url) {
+        if (!userData?.repos_url) {
             throw new Error(`Repos URL not found for user: ${username}`);
         }
 
         const reposResponse = await fetch(`https://api.github.com/users/${username}/repos`);
 
-        if (!reposResponse.ok) {
-            throw new Error(`Failed to fetch repositories: ${reposResponse.statusText}`);
+        if (!reposResponse?.ok) {
+            throw new Error(`Failed to fetch repositories: ${reposResponse?.statusText}`);
         }
 
-        const reposData = await reposResponse.json();
+        const reposData = await reposResponse?.json();
 
-        const languageCounts = reposData.reduce((acc, repo) => {
-            if (repo.language) {
-                acc[repo.language] = (acc[repo.language] || 0) + 1;
+        const languageCounts = reposData?.reduce((acc, repo) => {
+            if (repo?.language) {
+                acc[repo?.language] = (acc[repo?.language] || 0) + 1;
             }
             return acc;
         }, {});
 
-        const languages = Object.entries(languageCounts).map(([language, count]) => ({ language, count }));
+        const languages = Object.entries(languageCounts)?.map(([language, count]) => ({ language, count }))?.sort((acc, curr) => curr?.count - acc?.count);
 
-        const repoLatestTotal = reposData.reduce((acc, repo) => {
+        const repoLatestTotal = reposData?.reduce((acc, repo) => {
             const nowDate = new Date();
-            const repoDate = new Date(repo.created_at);
+            const repoDate = new Date(repo?.created_at);
 
             if (nowDate.getFullYear() - repoDate.getFullYear() === 0) {
                 acc += 1;
@@ -48,11 +48,11 @@ const getUserData = async (username) => {
 
         return {
             basicData: {
-                username: userData.login || "",
-                followers: userData.followers || 0,
-                public_repos: userData.public_repos || 0,
+                username: userData?.login || "",
+                followers: userData?.followers || 0,
+                public_repos: userData?.public_repos || 0,
                 repo_latest_total: repoLatestTotal || 0,
-                join_when: ConvertDays(userData.created_at) || "",
+                join_when: ConvertDays(userData?.created_at) || "",
             },
             languages,
         };
